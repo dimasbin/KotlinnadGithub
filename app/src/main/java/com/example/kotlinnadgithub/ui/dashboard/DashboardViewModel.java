7daef17085c08a7bf78790ab3cf9c09c25daa841
Bintang <bintangdimas4@gmail.com>
2021-09-02 16:54:04 +0700

    Tambah edit Text tapi belum ada logic

--- app/src/main/java/com/example/kotlinnadgithub/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/kotlinnadgithub/ui/dashboard/DashboardViewModel.java
@@ -1,16 +1,24 @@
 package com.example.kotlinnadgithub.ui.dashboard;
 
+import android.widget.EditText;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.kotlinnadgithub.R;
+
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private EditText user,pass;
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
+
+
+
     }
 
     public LiveData<String> getText() {
